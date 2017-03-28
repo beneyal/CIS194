b1336d495d490622842245e72f9941b2db692511
Ben Eyal <bene@post.bgu.ac.il>
2017-03-28 16:53:33 +0300

    Finish week 5

--- week5/Calc.hs
+++ week5/Calc.hs
@@ -1,15 +1,17 @@
+{-# LANGUAGE FlexibleInstances #-}
 module Calc where
 
 import ExprT
 import Parser
+import StackVM
 
 eval :: ExprT -> Integer
-eval (Lit n) = n
-eval (Add e1 e2) = eval e1 + eval e2
-eval (Mul e1 e2) = eval e1 * eval e2
+eval (ExprT.Lit n) = n
+eval (ExprT.Add e1 e2) = eval e1 + eval e2
+eval (ExprT.Mul e1 e2) = eval e1 * eval e2
 
 evalStr :: String -> Maybe Integer
-evalStr = fmap eval . parseExp Lit Add Mul
+evalStr = fmap eval . parseExp ExprT.Lit ExprT.Add ExprT.Mul
 
 class Expr a where
   lit :: Integer -> a
@@ -17,9 +19,9 @@ class Expr a where
   mul :: a -> a -> a
 
 instance Expr ExprT where
-  lit = Lit
-  add = Add
-  mul = Mul
+  lit = ExprT.Lit
+  add = ExprT.Add
+  mul = ExprT.Mul
 
 instance Expr Integer where
   lit = id
@@ -45,10 +47,10 @@ instance Expr Mod7 where
   add (Mod7 a) (Mod7 b) = Mod7 ((a + b) `mod` 7)
   mul (Mod7 a) (Mod7 b) = Mod7 ((a * b) `mod` 7)
 
-testExp :: Expr a => Maybe a
-testExp = parseExp lit add mul "(3*-4) + 5"
+instance Expr Program where
+  lit n = [PushI n]
+  add p1 p2 = p1 ++ p2 ++ [StackVM.Add]
+  mul p1 p2 = p1 ++ p2 ++ [StackVM.Mul]
 
-testInteger  = testExp :: Maybe Integer
-testBool     = testExp :: Maybe Bool
-testMM       = testExp :: Maybe MinMax
-testSat      = testExp :: Maybe Mod7
+compile :: String -> Maybe Program
+compile = parseExp lit add mul
