dfac8e1874243ee6e1f01168ac11864b83959b6f
Ben Eyal <bene@post.bgu.ac.il>
2017-03-30 22:14:29 +0300

    Finish week 6

--- week6/Fibonacci.hs
+++ week6/Fibonacci.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -fno-warn-missing-methods #-}
+{-# LANGUAGE FlexibleInstances #-}
 module Fibonacci where
 
 fib :: Integer -> Integer
@@ -36,3 +38,30 @@ interleaveStreams (Cons x xs) ys = Cons x (interleaveStreams ys xs)
 
 ruler :: Stream Integer
 ruler = foldr interleaveStreams undefined (map streamRepeat [0..])
+
+x :: Stream Integer
+x = Cons 0 (Cons 1 (streamRepeat 0))
+
+instance Num (Stream Integer) where
+  fromInteger n = Cons n (streamRepeat 0)
+  negate = streamMap negate
+  (Cons a as) + (Cons b bs) = Cons (a + b) (as + bs)
+  (Cons a as) * (Cons b bs) = Cons (a * b) (streamMap (* a) bs + as * (Cons b bs))
+
+instance Fractional (Stream Integer) where
+  (Cons a as) / (Cons b bs) = q
+    where q = Cons (a `div` b) (streamMap (1 `div` b *) (as - q * bs))
+
+fibs3 :: Stream Integer
+fibs3 = x / (1 - x - x^2)
+
+data Matrix = M Integer Integer Integer Integer
+
+instance Show Matrix where
+  show (M a b c d) = show a ++ " " ++ show b ++ "\n" ++ show c ++ " " ++ show d
+
+instance Num Matrix where
+  M a b c d * M e f g h = M (a * e + b * g) (a * f + b * h) (c * e + d * g) (c * f + d * h)
+
+fib4 :: Integer -> Integer
+fib4 n = let (M _ fib _ _) = (M 1 1 1 0) ^ n in fib
