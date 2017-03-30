155663bd59d4152b12a119e82412cbbc7d1ff94c
Ben Eyal <bene@post.bgu.ac.il>
2017-03-30 21:25:48 +0300

    Finish week 6, exercise 5

---
+++ week6/Fibonacci.hs
@@ -0,0 +1,38 @@
+module Fibonacci where
+
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+fibs1 :: [Integer]
+fibs1 = map fib [0..]
+
+fibs2 :: [Integer]
+fibs2 = [0, 1] ++ zipWith (+) fibs2 (drop 1 fibs2)
+
+data Stream a = Cons a (Stream a)
+
+streamToList :: Stream a -> [a]
+streamToList (Cons x xs) = x : streamToList xs
+
+instance Show a => Show (Stream a) where
+  show s = show $ take 20 (streamToList s)
+
+streamRepeat :: a -> Stream a
+streamRepeat x = Cons x (streamRepeat x)
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons x xs) = Cons (f x) (streamMap f xs)
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f x = Cons x (streamFromSeed f (f x))
+
+nats :: Stream Integer
+nats = streamFromSeed (+ 1) 1
+
+interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams (Cons x xs) ys = Cons x (interleaveStreams ys xs)
+
+ruler :: Stream Integer
+ruler = foldr interleaveStreams undefined (map streamRepeat [0..])
